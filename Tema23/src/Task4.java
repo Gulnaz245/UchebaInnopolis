d94fad371f5047a9202bc45877f5078d6e255ddd
Гульназ Хасанова <163520326+Gulnaz245@users.noreply.github.com>
2024-04-16 14:26:16 +0300

    Update Task4.java

--- Tema23/src/Task4.java
+++ Tema23/src/Task4.java
@@ -6,18 +6,20 @@ public class Task4 {
         Scanner sc=new Scanner(System.in);
         System.out.println("Введите число n=");
         int n=sc.nextInt();
-        double Sum=0.0;
-        for (double i=1; i<=n; i++){
-            Sum +=1/n;
+        double Sum=0;
+        for (int i=1; i<=n; i++){
             if (i==n) {
+                Sum +=Drobi(n);
                 System.out.printf("1/"+i+"="+Sum);
             }
             else {
+                Sum +=Drobi(i);
                 System.out.printf("1/"+i+"+");
             }
-            }
+        }
     }
-
+    public static double Drobi (int a){
+        return (double) 1 /a;
+    }
 }
-
 
