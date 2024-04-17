34f34903b9180c690fab370878b53cb4267c4c61
Гульназ Хасанова <163520326+Gulnaz245@users.noreply.github.com>
2024-04-17 09:55:31 +0300

    Create Name.java

---
+++ OOP1/phone/Name.java
@@ -0,0 +1,12 @@
+package phone;
+
+public class Name {
+    String ReceiveCall;
+
+    public void setReceiveCall(String ReceiveCall) {
+        this.ReceiveCall = ReceiveCall;
+    }
+    public String getReceiveCall(){
+        return ReceiveCall;
+    }
+}
